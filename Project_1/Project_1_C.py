aa36ba8fb8b5ae482b5a83700d3af06cca8e418f
Sadia <muddassirsadia123@gmail.com>
2025-05-30 08:22:58 -0700

    World quiz

--- Project_1/Project_1_C.py
+++ Project_1/Project_1_C.py
@@ -1,4 +1,27 @@
-print("Hello World!")
-print(5+8)
-name="Sadia"
-print("Hello, " + name+ "!"+ " How are you?")
+print("Hello Welcome to my World Quiz!")
+playing = input("Do you want to play? ")
+if playing.lower() != "yes":
+    quit()
+print("Okay! Let's play :)")
+answer = input("What is the capital of India? ").lower()
+if answer == "delhi":
+    print("Correct!")
+else: 
+    print("Incorrect! the correct answer is Delhi")
+answer == input("What is the capital of USA? ").lower()
+if answer == "washington d.c.":
+    print("Correct!")
+else: 
+    print("Incorrect! the correct answer is Washington D.C.")
+answer = input("What is the capital of Japan? ").lower()
+if answer == "tokyo":
+    print("Correct!")
+else:
+    print("Incorrect! the correct answer is Tokyo")
+answer = input("What is the capital of France? ").lower()
+if answer == "paris":
+    print("Correct!")
+else:
+    print("Incorrect! the correct answer is Paris")
+print("You have completed the quiz! Thank you for playing :)")
+# End of Project_1_C.py
