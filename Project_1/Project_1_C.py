c5ca8027660b6c46e2431cfdc6edc048c3ad43ff
Sadia <muddassirsadia123@gmail.com>
2025-05-30 17:25:25 -0700

    Added score

--- Project_1/Project_1_C.py
+++ Project_1/Project_1_C.py
@@ -3,25 +3,32 @@ playing = input("Do you want to play? ")
 if playing.lower() != "yes":
     quit()
 print("Okay! Let's play :)")
+score = 0
 answer = input("What is the capital of India? ").lower()
 if answer == "delhi":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect! the correct answer is Delhi")
 answer == input("What is the capital of USA? ").lower()
 if answer == "washington d.c.":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect! the correct answer is Washington D.C.")
 answer = input("What is the capital of Japan? ").lower()
 if answer == "tokyo":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect! the correct answer is Tokyo")
 answer = input("What is the capital of France? ").lower()
 if answer == "paris":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect! the correct answer is Paris")
 print("You have completed the quiz! Thank you for playing :)")
+print("I hope you enjoyed it!")
+print("You got", score, "out of 4 questions correct.")
 # End of Project_1_C.py
